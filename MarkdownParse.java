ad7527770a0b894a10727f2ea41c2364060fadb3
tonyroumi <aroumi@ucsd.edu>
2022-04-25 17:23:54 -0700

    edit

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,45 +12,21 @@ public class MarkdownParse {
 		ArrayList<String> toReturn = new ArrayList<>();
 		// Find the next [, then find ], then find (, then take up to the next )
 		int currentIndex = 0;
-		while(true) {
-			while(true) {
-                int openBracket = markdown.indexOf("[", currentIndex);
-                if(openBracket == -1) { 
-                    break; 
-                }
-                if(openBracket > 0) {
-                    if(markdown.charAt(openBracket - 1) == '!') {
-                        currentIndex = openBracket + 1;
-                        continue;
-                    }
-                }
-			int closeBracket = markdown.indexOf("]", openBracket);
-            if(closeBracket == -1) {
-                break;
-            }
-            int openParen = markdown.indexOf("(", closeBracket);
-            if(openParen == -1) {
-                break;
-            }
-            int closeParen = markdown.indexOf(")", openParen);
-            if(closeParen == -1) {
-                break;
-            }
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
-        }
-
-        return toReturn;
-    }
-			
+		while(currentIndex < markdown.length()) {
+			int nextOpenBracket = markdown.indexOf("[", currentIndex);
+			int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+			int openParan = markdown.indexOf("(", nextCloseBracket);
+			int closeParan = markdown.indexOf(")", openParan);
+			toReturn.add(markdown.substring(openParan + 1, closeParan));
+			currentIndex = closeParan + 1;
+		}
 		
-	
-    }
-	
-
+		return toReturn;
+		
+	}
 
     public static void main(String[] args) throws IOException {
-		String contents = Files.readString(Path.of("./test-file2.md"));
+		String contents = Files.readString(Path.of("./test-file.md"));
 	    ArrayList<String> links = getLinks(contents);
 		System.out.println(links);
 
