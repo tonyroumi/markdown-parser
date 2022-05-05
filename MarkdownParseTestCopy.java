d31b13dc8282d809a085d58a33e7c6ff6d06f19b
tonyroumi <103224803+tonyroumi@users.noreply.github.com>
2022-05-05 16:21:44 -0700

    Create MarkdownParseTestCopy.java

---
+++ MarkdownParseTestCopy.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
+
+public class MarkdownParseTest {
+  @Test
+  public void addition() {
+    assertEquals(2, 1 + 1);
+  }
+  
+  @Test
+  public void linksTest() throws IOException {
+    Path fileName = Path.of("test-file.md");
+    String content = Files.readString(fileName);
+    ArrayList<String> links = MarkdownParse.getLinks(content);
+    ArrayList<String> correctOutput = new ArrayList<String>();
+    correctOutput.add("https://something.com");
+    correctOutput.add("some-thing.html");
+    assertEquals(correctOutput, links);
+  }
+
+  @Test
+  public void test2() throws IOException {
+    Path fileName = Path.of("test-file2.md");
+    String content = Files.readString(fileName);
+    ArrayList<String> links = MarkdownParse.getLinks(content);
+    ArrayList<String> correctOutput = new ArrayList<String>();
+    correctOutput.add("what's up");
+    assertEquals(correctOutput, links);
+  }
+
+  @Test
+  public void test3() throws IOException {
+    Path fileName = Path.of("test-file3.md");
+    String content = Files.readString(fileName);
+    ArrayList<String> links = MarkdownParse.getLinks(content);
+    ArrayList<String> correctOutput = new ArrayList<String>();
+    assertEquals(correctOutput, links);
+  }
+}
+
+
+
+//javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
+//java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
