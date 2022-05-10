4e91080a2942001a918cacadf36fe0ad0230ead7
tonyroumi <aroumi@ucsd.edu>
2022-05-09 19:07:09 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -47,7 +47,7 @@ public class MarkdownParseTest {
 
 @Test
 public void test4() throws IOException {
-  Path fileName = Path.of("test-file3.md");
+  Path fileName = Path.of("test-file5.md");
   String content = Files.readString(fileName);
   ArrayList<String> links = MarkdownParse.getLinks(content);
   ArrayList<String> correctOutput = new ArrayList<String>();
